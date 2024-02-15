bca45dd6db9759eb4d491ff83ece4747289b72bc
Dmitry Solovov <cyradin@protonmail.com>
2024-02-16 02:22:06 +0300

    Add Bitmap8 and Bitmap16

--- bitmap_16.go
+++ bitmap_16.go
@@ -6,20 +6,18 @@ import (
 	"strings"
 )
 
-type Bitmap Bitmap64
-
-type Bitmap64 []uint64
+type Bitmap16 []uint16
 
 // Set set n-th bit to 1
-func (b *Bitmap64) Set(n uint32) {
-	block, bit := n>>6, n%64
+func (b *Bitmap16) Set(n uint32) {
+	block, bit := n>>4, n%16
 	b.grow(block)
 	(*b)[block] |= (1 << bit)
 }
 
 // Remove set n-th bit to 0
-func (b *Bitmap64) Remove(n uint32) {
-	block, bit := n>>6, n%64
+func (b *Bitmap16) Remove(n uint32) {
+	block, bit := n>>4, n%16
 	if uint32(len(*b)) <= block {
 		return
 	}
@@ -27,14 +25,14 @@ func (b *Bitmap64) Remove(n uint32) {
 }
 
 // Xor invert n-th bit
-func (b *Bitmap64) Xor(n uint32) {
-	block, val := n>>6, n%64
+func (b *Bitmap16) Xor(n uint32) {
+	block, val := n>>4, n%16
 	b.grow(block)
 	(*b)[block] ^= (1 << val)
 }
 
 // IsEmpty check if the bitmap has any bit set to 1
-func (b *Bitmap64) IsEmpty() bool {
+func (b *Bitmap16) IsEmpty() bool {
 	for i := range *b {
 		if (*b)[i] > 0 {
 			return false
@@ -45,8 +43,8 @@ func (b *Bitmap64) IsEmpty() bool {
 }
 
 // Has check if n-th bit is set to 1
-func (b *Bitmap64) Has(n uint32) bool {
-	block, val := n>>6, n%64
+func (b *Bitmap16) Has(n uint32) bool {
+	block, val := n>>4, n%16
 	if uint32(len(*b)) <= block {
 		return false
 	}
@@ -55,7 +53,7 @@ func (b *Bitmap64) Has(n uint32) bool {
 }
 
 // CountDiff count different bits in two bitmaps
-func (b *Bitmap64) CountDiff(b2 Bitmap64) int {
+func (b *Bitmap16) CountDiff(b2 Bitmap16) int {
 	diff := 0
 	max := len(*b)
 	if len(b2) > max {
@@ -64,22 +62,22 @@ func (b *Bitmap64) CountDiff(b2 Bitmap64) int {
 
 	for i := 0; i < max; i++ {
 		if len(b2) <= i {
-			diff += bits.OnesCount64((*b)[i])
+			diff += bits.OnesCount16((*b)[i])
 			continue
 		}
 		if len(*b) <= i {
-			diff += bits.OnesCount64((b2)[i])
+			diff += bits.OnesCount16((b2)[i])
 			continue
 		}
 
-		diff += bits.OnesCount64((*b)[i] ^ b2[i])
+		diff += bits.OnesCount16((*b)[i] ^ b2[i])
 	}
 
 	return diff
 }
 
 // Or in-place OR operation with another bitmap
-func (b *Bitmap64) Or(b2 Bitmap64) {
+func (b *Bitmap16) Or(b2 Bitmap16) {
 	b.grow(uint32(len(b2) - 1))
 	for i := 0; i < len(b2); i++ {
 		if b2[i] == 0 {
@@ -90,14 +88,14 @@ func (b *Bitmap64) Or(b2 Bitmap64) {
 }
 
 // And in-place And operation with another bitmap
-func (b *Bitmap64) And(b2 Bitmap64) {
+func (b *Bitmap16) And(b2 Bitmap16) {
 	for i := 0; i < len(b2) && i < len(*b); i++ {
 		(*b)[i] &= b2[i]
 	}
 }
 
 // Shrink remove zero elements at the end of the map
-func (b *Bitmap64) Shrink() {
+func (b *Bitmap16) Shrink() {
 	shrinkedIndex := len(*b)
 	for i := len(*b) - 1; i >= 0; i-- {
 		if (*b)[i] != 0 {
@@ -107,15 +105,15 @@ func (b *Bitmap64) Shrink() {
 	}
 
 	if shrinkedIndex != len(*b) {
-		newSlice := make(Bitmap64, shrinkedIndex)
+		newSlice := make(Bitmap16, shrinkedIndex)
 		copy(newSlice, (*b)[:shrinkedIndex])
 		*b = newSlice
 	}
 }
 
 // Clone create a copy of the bitmap
-func (b *Bitmap64) Clone() Bitmap64 {
-	clone := make(Bitmap64, len(*b))
+func (b *Bitmap16) Clone() Bitmap16 {
+	clone := make(Bitmap16, len(*b))
 	copy(clone, *b)
 
 	return clone
@@ -123,11 +121,11 @@ func (b *Bitmap64) Clone() Bitmap64 {
 
 // Range call the passed callback with all bits set to 1.
 // If the callback returns false, the method exits
-func (b *Bitmap64) Range(f func(n uint32) bool) {
+func (b *Bitmap16) Range(f func(n uint32) bool) {
 	for i, block := range *b {
 		for block != 0 {
-			tz := bits.TrailingZeros64(block)
-			bitIndex := uint32(i*64 + tz)
+			tz := bits.TrailingZeros16(block)
+			bitIndex := uint32(i*16 + tz)
 
 			if !f(bitIndex) {
 				return
@@ -138,11 +136,11 @@ func (b *Bitmap64) Range(f func(n uint32) bool) {
 	}
 }
 
-func (b *Bitmap64) String() string {
+func (b *Bitmap16) String() string {
 	var sb strings.Builder
 
 	for i := range *b {
-		sb.WriteString(strconv.FormatUint((*b)[i], 10))
+		sb.WriteString(strconv.FormatUint(uint64((*b)[i]), 10))
 		if i != len(*b)-1 {
 			sb.WriteString("|")
 		}
@@ -151,25 +149,25 @@ func (b *Bitmap64) String() string {
 	return sb.String()
 }
 
-func FromString(str string) (Bitmap64, error) {
+func FromString16(str string) (Bitmap16, error) {
 	if str == "" {
-		return Bitmap64{}, nil
+		return Bitmap16{}, nil
 	}
 
 	nums := strings.Split(str, "|")
-	result := make(Bitmap64, 0, len(nums))
+	result := make(Bitmap16, 0, len(nums))
 	for _, num := range nums {
-		v, err := strconv.ParseUint(num, 10, 64)
+		v, err := strconv.ParseUint(num, 10, 16)
 		if err != nil {
 			return nil, err
 		}
-		result = append(result, v)
+		result = append(result, uint16(v))
 	}
 	return result, nil
 }
 
-func (b *Bitmap64) grow(length uint32) {
+func (b *Bitmap16) grow(length uint32) {
 	if length+1 > uint32(len(*b)) {
-		*b = append(*b, make(Bitmap64, length+1-uint32(len(*b)))...)
+		*b = append(*b, make(Bitmap16, length+1-uint32(len(*b)))...)
 	}
 }
--- bitmap_16_test.go
+++ bitmap_16_test.go
@@ -6,54 +6,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Benchmark_Bitmap64_Set(b *testing.B) {
-	var bm Bitmap64
+func Benchmark_Bitmap16_Set(b *testing.B) {
+	var bm Bitmap16
 	for i := 0; i < b.N; i++ {
 		bm.Set(1)
 	}
 }
 
-func Test_Bitmap64_Set(t *testing.T) {
+func Test_Bitmap16_Set(t *testing.T) {
 	t.Run("must set the specified bit", func(t *testing.T) {
 		t.Run("0", func(t *testing.T) {
-			var b Bitmap64
+			var b Bitmap16
 			b.Set(0)
-			assert.Equal(t, Bitmap64{1}, b)
+			assert.Equal(t, Bitmap16{1}, b)
 		})
 		t.Run("2", func(t *testing.T) {
-			var b Bitmap64
+			var b Bitmap16
 			b.Set(2)
-			assert.Equal(t, Bitmap64{4}, b)
+			assert.Equal(t, Bitmap16{4}, b)
 		})
 		t.Run("0,2", func(t *testing.T) {
-			var b Bitmap64
+			var b Bitmap16
 			b.Set(0)
 			b.Set(2)
-			assert.Equal(t, Bitmap64{5}, b)
+			assert.Equal(t, Bitmap16{5}, b)
 		})
 
-		t.Run("63", func(t *testing.T) {
-			var b Bitmap64
-			b.Set(63)
-			assert.Equal(t, Bitmap64{9223372036854775808}, b)
+		t.Run("15", func(t *testing.T) {
+			var b Bitmap16
+			b.Set(15)
+			assert.Equal(t, Bitmap16{32768}, b)
 		})
 
-		t.Run("64", func(t *testing.T) {
-			var b Bitmap64
-			b.Set(64)
-			assert.Equal(t, Bitmap64{0, 1}, b)
+		t.Run("16", func(t *testing.T) {
+			var b Bitmap16
+			b.Set(16)
+			assert.Equal(t, Bitmap16{0, 1}, b)
 		})
 	})
 	t.Run("must do nothing if the specified bit is already == 1", func(t *testing.T) {
-		var b Bitmap64
+		var b Bitmap16
 		b.Set(0)
 		b.Set(0)
-		assert.Equal(t, Bitmap64{1}, b)
+		assert.Equal(t, Bitmap16{1}, b)
 	})
 }
 
-func Test_Bitmap64_Remove(t *testing.T) {
-	var b Bitmap64
+func Test_Bitmap16_Remove(t *testing.T) {
+	var b Bitmap16
 
 	b.Remove(0)
 	assert.Nil(t, b)
@@ -62,51 +62,51 @@ func Test_Bitmap64_Remove(t *testing.T) {
 	b.Set(1)
 	b.Set(100)
 	b.Remove(100)
-	assert.Equal(t, Bitmap64{3, 0}, b)
+	assert.Equal(t, Bitmap16{3, 0, 0, 0, 0, 0, 0}, b)
 	b.Remove(1)
-	assert.Equal(t, Bitmap64{1, 0}, b)
+	assert.Equal(t, Bitmap16{1, 0, 0, 0, 0, 0, 0}, b)
 	b.Remove(0)
-	assert.Equal(t, Bitmap64{0, 0}, b)
+	assert.Equal(t, Bitmap16{0, 0, 0, 0, 0, 0, 0}, b)
 }
 
-func Benchmark_Bitmap64_Xor(b *testing.B) {
-	var bm Bitmap64
+func Benchmark_Bitmap16_Xor(b *testing.B) {
+	var bm Bitmap16
 	for i := 0; i < b.N; i++ {
 		bm.Xor(1)
 	}
 }
 
-func Test_Bitmap64_Xor(t *testing.T) {
+func Test_Bitmap16_Xor(t *testing.T) {
 	t.Run("must invert the specified bit", func(t *testing.T) {
-		var b Bitmap64
+		var b Bitmap16
 		b.Xor(0)
-		assert.Equal(t, Bitmap64{1}, b)
+		assert.Equal(t, Bitmap16{1}, b)
 		b.Xor(1)
-		assert.Equal(t, Bitmap64{3}, b)
+		assert.Equal(t, Bitmap16{3}, b)
 		b.Xor(0)
-		assert.Equal(t, Bitmap64{2}, b)
+		assert.Equal(t, Bitmap16{2}, b)
 		b.Xor(1)
-		assert.Equal(t, Bitmap64{0}, b)
+		assert.Equal(t, Bitmap16{0}, b)
 	})
 }
 
-func Test_Bitmap64_IsEmpty(t *testing.T) {
+func Test_Bitmap16_IsEmpty(t *testing.T) {
 	t.Run("must return true if it's empty", func(t *testing.T) {
-		var b Bitmap64
+		var b Bitmap16
 		assert.True(t, b.IsEmpty())
 		b.Xor(1)
 		b.Xor(1)
 		assert.True(t, b.IsEmpty())
 	})
 	t.Run("must return false if it is not empty", func(t *testing.T) {
-		var b Bitmap64
+		var b Bitmap16
 		b.Set(1)
 		assert.False(t, b.IsEmpty())
 	})
 }
 
-func Test_Bitmap64_Has(t *testing.T) {
-	var b Bitmap64
+func Test_Bitmap16_Has(t *testing.T) {
+	var b Bitmap16
 	assert.False(t, b.Has(0))
 	b.Xor(0)
 	assert.True(t, b.Has(0))
@@ -114,8 +114,8 @@ func Test_Bitmap64_Has(t *testing.T) {
 	assert.False(t, b.Has(0))
 }
 
-func Benchmark_Bitmap64_CountDiff(b *testing.B) {
-	var b1, b2 Bitmap64
+func Benchmark_Bitmap16_CountDiff(b *testing.B) {
+	var b1, b2 Bitmap16
 	b1.Set(1)
 	b1.Set(2)
 	b2.Set(31)
@@ -124,16 +124,16 @@ func Benchmark_Bitmap64_CountDiff(b *testing.B) {
 	}
 }
 
-func Test_Bitmap64_CountDiff(t *testing.T) {
+func Test_Bitmap16_CountDiff(t *testing.T) {
 	t.Run("must return 0 if bitmaps are equal", func(t *testing.T) {
-		var b1, b2 Bitmap64
+		var b1, b2 Bitmap16
 		b1.Set(1)
 		b2.Set(1)
 		assert.Equal(t, 0, b1.CountDiff(b2))
 	})
 
 	t.Run("must return correct count of different bits", func(t *testing.T) {
-		var b1, b2 Bitmap64
+		var b1, b2 Bitmap16
 		b1.Set(1)
 		b1.Set(2)
 		b1.Set(64)
@@ -146,8 +146,8 @@ func Test_Bitmap64_CountDiff(t *testing.T) {
 	})
 }
 
-func Test_Bitmap64_Or(t *testing.T) {
-	var b1, b2 Bitmap64
+func Test_Bitmap16_Or(t *testing.T) {
+	var b1, b2 Bitmap16
 	b1.Set(0)
 	b1.Set(1)
 	b1.Set(100)
@@ -160,7 +160,7 @@ func Test_Bitmap64_Or(t *testing.T) {
 
 	b1.Or(b2)
 
-	assert.Equal(t, Bitmap64{7, 206158430208, 1}, b1)
+	assert.Equal(t, Bitmap16{7, 0, 0, 0, 0, 0, 48, 0, 1}, b1)
 	assert.True(t, b1.Has(0))
 	assert.True(t, b1.Has(1))
 	assert.True(t, b1.Has(2))
@@ -170,8 +170,8 @@ func Test_Bitmap64_Or(t *testing.T) {
 	assert.False(t, b2.Has(100))
 }
 
-func Test_Bitmap64_And(t *testing.T) {
-	var b1, b2 Bitmap64
+func Test_Bitmap16_And(t *testing.T) {
+	var b1, b2 Bitmap16
 	b1.Set(0)
 	b1.Set(1)
 	b1.Set(100)
@@ -184,7 +184,7 @@ func Test_Bitmap64_And(t *testing.T) {
 
 	b1.And(b2)
 
-	assert.Equal(t, Bitmap64{3, 0}, b1)
+	assert.Equal(t, Bitmap16{3, 0, 0, 0, 0, 0, 0}, b1)
 	assert.True(t, b1.Has(0))
 	assert.True(t, b1.Has(1))
 	assert.False(t, b1.Has(2))
@@ -194,29 +194,29 @@ func Test_Bitmap64_And(t *testing.T) {
 	assert.True(t, b2.Has(2))
 }
 
-func Test_Bitmap64_Shrink(t *testing.T) {
-	var b Bitmap64
+func Test_Bitmap16_Shrink(t *testing.T) {
+	var b Bitmap16
 	b.Set(1)
 	b.Set(100)
 	b.Remove(100)
 	b.Shrink()
-	assert.Equal(t, Bitmap64{2}, b)
+	assert.Equal(t, Bitmap16{2}, b)
 }
 
-func Test_Bitmap64_Clone(t *testing.T) {
-	var b1 Bitmap64
+func Test_Bitmap16_Clone(t *testing.T) {
+	var b1 Bitmap16
 	b1.Set(0)
 	b2 := b1.Clone()
 
 	assert.Equal(t, b1, b2)
 
 	b2.Set(2)
-	assert.Equal(t, Bitmap64{5}, b2)
-	assert.Equal(t, Bitmap64{1}, b1)
+	assert.Equal(t, Bitmap16{5}, b2)
+	assert.Equal(t, Bitmap16{1}, b1)
 }
 
-func Test_Bitmap64_Range(t *testing.T) {
-	var b1 Bitmap64
+func Test_Bitmap16_Range(t *testing.T) {
+	var b1 Bitmap16
 	b1.Set(0)
 	b1.Set(1)
 	b1.Set(2)
@@ -236,37 +236,37 @@ func Test_Bitmap64_Range(t *testing.T) {
 	assert.Equal(t, []uint32{0, 1, 2, 1000}, items)
 }
 
-func Benchmark_Bitmap64_String(b *testing.B) {
-	bm := Bitmap64{0, 5, 1000}
+func Benchmark_Bitmap16_String(b *testing.B) {
+	bm := Bitmap16{0, 5, 1000}
 	for i := 0; i < b.N; i++ {
 		_ = bm.String()
 	}
 }
 
-func Test_Bitmap64_String(t *testing.T) {
-	b := Bitmap64{}
+func Test_Bitmap16_String(t *testing.T) {
+	b := Bitmap16{}
 	assert.Equal(t, "", b.String())
 
-	b = Bitmap64{0, 5, 100}
+	b = Bitmap16{0, 5, 100}
 	assert.Equal(t, "0|5|100", b.String())
 }
 
-func Test_FromString(t *testing.T) {
+func Test_FromString16(t *testing.T) {
 	t.Run("must return error if unable to parse the string", func(t *testing.T) {
-		_, err := FromString("qwe")
+		_, err := FromString16("qwe")
 		assert.Error(t, err)
 	})
 	t.Run("must parse the string correctly", func(t *testing.T) {
-		v, err := FromString("")
+		v, err := FromString16("")
 		assert.Nil(t, err)
-		assert.Equal(t, Bitmap64{}, v)
+		assert.Equal(t, Bitmap16{}, v)
 
-		v, err = FromString("0")
+		v, err = FromString16("0")
 		assert.Nil(t, err)
-		assert.Equal(t, Bitmap64{0}, v)
+		assert.Equal(t, Bitmap16{0}, v)
 
-		v, err = FromString("0|5")
+		v, err = FromString16("0|5")
 		assert.Nil(t, err)
-		assert.Equal(t, Bitmap64{0, 5}, v)
+		assert.Equal(t, Bitmap16{0, 5}, v)
 	})
 }
